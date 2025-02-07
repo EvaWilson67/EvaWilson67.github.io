3ebfab58b8623c1048a1f6a406553a568df51857
EvaWilson67 <143644462+EvaWilson67@users.noreply.github.com>
2025-02-06 20:08:25 -0500

    Finishing Assignment5

--- csce242/assignments/assignment05/script.js
+++ csce242/assignments/assignment05/script.js
@@ -1,21 +1,22 @@
+/*
+@author Eva Wilson
+*/
 
-
+//Adding hello paragraphs to the hello box
 document.getElementById("hello-grid").onclick = () => {
   //https://stackoverflow.com/questions/41764061/adding-text-to-an-existing-text-element-in-javascript-via-dom
   document.getElementById("hello-grid").innerHTML += "<p>hello</p>";
 };
 
-
-
-//portiaportia.github.io/csce242-fall2024/exercises/07_js_nav/index.html
-document.getElementById("txt-color").onchange = (e) => {
+document.getElementById("txt-color").onchange = (event) => {
+//portiaportia.github.io/csce242-fall2024/exercises/07_js_nav/index.html line 17
   document
     .querySelector(":root")
-    .style.setProperty("--star-color", e.target.value);
+    .style.setProperty("--star-color", event.target.value);
 };
 
 document.getElementById("image-change").onclick = () => {
   const img = document.getElementById("changeable-image");
-  https://stackoverflow.com/questions/1077041/refresh-image-with-a-new-one-at-the-same-url
-  img.src="https://picsum.photos/200?" + new Date().getTime();
+  //stackoverflow.com/questions/1077041/refresh-image-with-a-new-one-at-the-same-url
+  https: img.src = "https://picsum.photos/200?" + new Date().getTime();
 };
