3734d8a1f3c7d8b7273781321930fef7f263f413
EvaWilson67 <143644462+EvaWilson67@users.noreply.github.com>
2025-02-06 19:44:09 -0500

    Finishing Assignment05

--- csce242/assignments/assignment05/script.js
+++ csce242/assignments/assignment05/script.js
@@ -1,18 +1,21 @@
 
 
-document.getElementById("hello-grid").onclick = () =>{
-    //https://stackoverflow.com/questions/41764061/adding-text-to-an-existing-text-element-in-javascript-via-dom
-    document.getElementById("hello-grid").innerHTML+="<p>hello</p>";
-}
+document.getElementById("hello-grid").onclick = () => {
+  //https://stackoverflow.com/questions/41764061/adding-text-to-an-existing-text-element-in-javascript-via-dom
+  document.getElementById("hello-grid").innerHTML += "<p>hello</p>";
+};
 
+
+
+//portiaportia.github.io/csce242-fall2024/exercises/07_js_nav/index.html
 document.getElementById("txt-color").onchange = (e) => {
-    //document.getElementById("ball").style.setProperty("background-color", e.target.value);
-    document.querySelector(":root").style.setProperty("--star-color", e.target.value);
-}
+  document
+    .querySelector(":root")
+    .style.setProperty("--star-color", e.target.value);
+};
 
-//changeable-image
-const img = document.getElementById("changeable-image");
-img.addEventListener('click', function(){
-    // Append a timestamp to force a new image to load... ONLINE
-    img.src = `https://picsum.photos/200?random=${Date.now()}`;
-})
+document.getElementById("image-change").onclick = () => {
+  const img = document.getElementById("changeable-image");
+  https://stackoverflow.com/questions/1077041/refresh-image-with-a-new-one-at-the-same-url
+  img.src="https://picsum.photos/200?" + new Date().getTime();
+};
--- csce242/exercises/06_js_intro/script.js
+++ csce242/exercises/06_js_intro/script.js
@@ -1,26 +1,55 @@
 // alert("hello");
 // console.log("hi me");
 
-
 //Do stuff when button clicked
 //Parenthesis mean NOW**
-document.getElementById("btn-click-me").onclick = () =>{
-    console.log("Wow!");
+document.getElementById("btn-click-me").onclick = () => {
+  console.log("Wow!");
 };
-
 
 //You need to only use const, do lets when it is non constant like 4 loops and shi...
 //If we need to change more, we can call it a const
-document.getElementById("result").innerHTML="Hi <strong> you </strong>";
+document.getElementById("result").innerHTML = "Hi <strong> you </strong>";
 
-document.getElementById("btn-color").onclick = () =>{
-    console.log("Hello World");
+document.getElementById("btn-color").onclick = () => {
+  console.log("Hello World");
 
-    const colorP = document.getElementById("color-p");
-    colorP.innerHTML="Goodbye";
+  const colorP = document.getElementById("color-p");
+  colorP.innerHTML = "Goodbye";
+  colorP.classList.toggle("sad");
+};
 
+//Button Happy
+document.getElementById("btn-happy").onclick = () => {
+  const colorMe = document.getElementById("color-text");
+  colorMe.classList.remove("hidden"); //added from portia
+  colorMe.classList.add("happy");
+  colorMe.innerHTML = "Good Times";
+};
 
-    colorP.classList.toggle("sad");
+//Button Happy
+document.getElementById("btn-sad").onclick = () => {
+  const colorMe = document.getElementById("color-text");
+  colorMe.classList.remove("hidden"); //added from portia
 
-}
+  colorMe.classList.remove("happy"); //Gets rid weird happy thing...
 
+  colorMe.classList.add("sad");
+  colorMe.innerHTML = "Bad Times";
+};
+
+document.getElementById("btn-clear").onclick = () => {
+  document.getElementById("color-text").classList.add("hidden");
+};
+
+// document.getElementById("txt-emotion").onkeydown = () =>{
+//     // console.log("woah");
+//    // const emotion = document.getElementById("txt-emotion").value;
+//    }
+
+document.getElementById("txt-emotion").onkeyup = (event) => {
+  const emotion = event.currentTarget;
+  //   console.log(emotion);
+  document.getElementById("emotional-message").innerHTML =
+    "You are feeling " + event.currentTarget.value;
+};
