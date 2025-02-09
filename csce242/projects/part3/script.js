0a52bd69487ea181c8856adea42ac091ecabdb8e
EvaWilson67 <143644462+EvaWilson67@users.noreply.github.com>
2025-02-09 11:21:15 -0500

    Adding project 3

--- csce242/projects/part3/script.js
+++ csce242/projects/part3/script.js
@@ -1,42 +1,29 @@
 
 
-
+//For future contacting...
 // //key down
 // document.getElementById("txt-emotion").onkeyup = (event) => {
 //     //const emotion = document.getElementById("txt-emotion").value;
-//     // document.getElementById("emotional-message").innerHTML = 
+//     // document.getElementById("emotional-message").innerHTML =
 //     // "You are feeling " + event.currentTarget.value;
 // }
 
-
-// const element = document.getElementById('yourElementId');
-
-// if (element) {
-//   element.onclick = function() {
-//     // Your code here
-//   };
-// } else {
-//   console.error('Element with ID "yourElementId" not found.');
-// }
-
-const element = document.getElementById('btn-submit');
-
+const element = document.getElementById("btn-submit");
 if (element) {
-    document.getElementById("btn-submit").onclick = () => {
-        document.getElementById("submission-response").innerHTML="Submission Sent!";
-        // console.log("WOW!");
-    };
+  document.getElementById("btn-submit").onclick = () => {
+    document.getElementById("submission-response").innerHTML =
+      "Submission Sent!";
+  };
 }
 
-
-
+// document.getElementById("txt-box").onkeyup = (event) => {
+//   this.style.height = this.scrollHeight + "px";
+// };
 
 document.getElementById("hidden-2").onmouseover = () => {
-    document.getElementById("hidden-2").classList.add("unhidden-title");
-    console.log("Fukcs!");
+  document.getElementById("hidden-2").classList.add("unhidden-title");
 };
 
 document.getElementById("hidden-2").onmouseleave = () => {
-    document.getElementById("hidden-2").classList.remove("unhidden-title");
-    console.log("WOW!");
+  document.getElementById("hidden-2").classList.remove("unhidden-title");
 };
