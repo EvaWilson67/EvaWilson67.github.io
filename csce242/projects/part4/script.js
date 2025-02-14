27b70cd9485253c5f6847158cca1264a392a4c49
EvaWilson67 <143644462+EvaWilson67@users.noreply.github.com>
2025-02-13 20:20:50 -0500

    project

--- csce242/projects/part4/script.js
+++ csce242/projects/part4/script.js
@@ -15,7 +15,6 @@ if (element) {
 }
 
 const garden = document.getElementById("hidden-2");
-
 if (garden) {
   document.getElementById("hidden-2").onmouseover = () => {
     document.getElementById("hidden-2").classList.add("unhidden-title");
