562568843fa7261ba42177e0dd66c545cc05e907
EvaWilson67 <143644462+EvaWilson67@users.noreply.github.com>
2025-02-13 19:41:29 -0500

    project

--- csce242/exercises/07_js_toggle/script.js
+++ csce242/exercises/07_js_toggle/script.js
@@ -1,7 +1,7 @@
 //do stuff when button clicked
 document.getElementById("btn-click-me").onclick = () => {
   console.log("WOW!");
-  document.getElementById("result").innerHTML="Hi Portia";
+  document.getElementById("result").innerHTML = "Hi Portia";
 };
 
 //change color
@@ -9,8 +9,7 @@ document.getElementById("btn-color").onclick = () => {
   const messageP = document.getElementById("message");
   messageP.innerHTML = "Good bye";
   messageP.classList.toggle("sad");
-
-}
+};
 
 //happy script
 document.getElementById("btn-happy").onclick = () => {
@@ -35,31 +34,83 @@ document.getElementById("btn-clear").onclick = () => {
 //key down
 document.getElementById("txt-emotion").onkeyup = (event) => {
   //const emotion = document.getElementById("txt-emotion").value;
-  document.getElementById("emotional-message").innerHTML = 
-  "You are feeling " + event.currentTarget.value;
-}
+  document.getElementById("emotional-message").innerHTML =
+    "You are feeling " + event.currentTarget.value;
+};
 
 //toggling the nav
 document.getElementById("toggle-nav").onclick = () => {
   document.getElementById("nav-items").classList.toggle("hide-small");
 };
 
-//show color message 
+//show color message
 document.getElementById("btn-choose-color").onclick = () => {
   const color = document.getElementById("txt-color").value.toLowerCase().trim();
   let mood = "";
 
-  if(color == "blue"){
-      mood = "grumpy";
-  } 
-  else if(color == "yellow"){
-      mood = "mellow";
-  }
-  else {
-      mood = "undefined";
+  if (color == "blue") {
+    mood = "grumpy";
+  } else if (color == "yellow") {
+    mood = "mellow";
+  } else {
+    mood = "undefined";
   }
 
-  
-  document.getElementById("color-message").innerHTML = 
-  `You chose ${color}. You are feeling ${mood}.`
+  document.getElementById(
+    "color-message"
+  ).innerHTML = `You chose ${color}. You are feeling ${mood}.`;
+};
+
+//Moving the circle
+//needs to be set outside otherwise it would be reset each time
+let pos = 0;
+
+const changeCirclePos = (increment) => {
+  pos += increment;
+  document.getElementById("circle").style.setProperty("top", pos + "px");
+};
+
+document.getElementById("btn-down").onclick = () => {
+  // pos+=10;
+  changeCirclePos(10);
+};
+
+document.getElementById("btn-up").onclick = () => {
+  // pos-=10;
+  changeCirclePos(-10);
+};
+
+document.getElementById("btn-pick-color").onchange = (event) => {
+  const color = event.currentTarget.value;
+  // document.getElementById("circle").style.setProperty("background", color);
+  document.getElementById("circle").style.setProperty("--circle-color", color);
+};
+
+// Donations thermometer
+const GOAL = 1000;
+let donations = 0;
+
+document.getElementById("goal-display").innerHTML = `Goal $${GOAL}`;
+
+//on page load hide the error
+//when add click, if donation is negative display error, otherwise update the donations and display in console
+
+document.getElementById("btn-add-donation").onclick = () => {
+  const donation = document.getElementById("txt-donation").value;
+  document.getElementById("donation-error").classList.add("hidden");
+
+  if (isNaN(donation) || donation <= 0) {
+    document.getElementById("donation-error").classList.remove("hidden");
+    return;
+  }
+  //Update donation
+  donations += donations;
+  donationPercent = (donations / GOAL) * 100;
+  //show donation message
+  document.getElementById("document-message").innerHTML = `$${
+    GOAL - donations
+  } to go!`;
+
+  //update thermometer
+  document.getElementById("thermometer").style.setProperty("--donation-percent", donationPercent + "%")
 };
--- csce242/projects/part4/script.js
+++ csce242/projects/part4/script.js
@@ -14,78 +14,22 @@ if (element) {
   };
 }
 
-// // document.getElementById("txt-box").onkeyup = (event) => {
-// //   this.style.height = this.scrollHeight + "px";
-// // };
 
-// document.getElementById("hidden-2").onmouseover = () => {
-//   document.getElementById("hidden-2").classList.add("unhidden-title");
-// };
+document.getElementById("hidden-2").onmouseover = () => {
+  document.getElementById("hidden-2").classList.add("unhidden-title");
+};
 
-// document.getElementById("hidden-2").onmouseleave = () => {
-//   document.getElementById("hidden-2").classList.remove("unhidden-title");
-// };
-
-// document.getElementById("toggle-nav").onclick = () => {
-//   document.getElementById("nav-items").classList.toggle("hide");
-//   // document.getElementById("nav-items").classList.remove("hidden");
-//   console.log("FUCKV");
-// }
-
-// const navigation = document.getElementById("toggle-nav");
-// if (navigation) {
-//   document.getElementById("toggle-nav").onclick = () => {
-//     // document.getElementById("submission-response").innerHTML =
-//     //   "Submission Sent!";
-//     // document.getElementById("nav-items").classList.toggle("hide");
-//     console.log("FUCKV");
-//   };
-// }
-
-// document.addEventListener('DOMContentLoaded', function () {
-//   const navigation = document.getElementById("toggle-nav");
-//   if (navigation) {
-//     navigation.onclick = () => {
-//       console.log("FUCKV");
-//     };
-//   }
-// });
-
-// document.addEventListener('DOMContentLoaded', function () {
-//   const button = document.getElementById("work-please");
-//   console.log(button); // This will log the button element if it is found
-//   if (button) {
-//     button.onclick = () => {
-//       console.log("FUCKV");
-//     };
-//   } else {
-//     console.log("Button not found!");
-//   }
-// });
-
-// const button = document.getElementById("work-please");
-// console.log(button); // This will log the button element if it is found
-// if (button) {
-//   button.onclick = () => {
-//     console.log("FUCKV");
-//   };
-// } else {
-//   console.log("Button not found!");
-// }
+document.getElementById("hidden-2").onmouseleave = () => {
+  document.getElementById("hidden-2").classList.remove("unhidden-title");
+};
 
 const navigation = document.getElementById("toggle-nav");
 console.log(navigation);
 if (navigation) {
   navigation.onclick = () => {
     document.getElementById("nav-items").classList.toggle("hide");
-    console.log("FUCKV");
   };
 } else {
   console.log("Button not found!");
 }
 
-// document.getElementById("nav-items").onclick = () => {
-//   // document.getElementById("nav-items").classList.toggle("hidden");
-//   // document.getElementById("nav-items").classList.remove("hidden");
-//   console.log("FUCKV");
-// }
