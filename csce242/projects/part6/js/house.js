29465e80a245f43fc0978006185c59f055a25d71
EvaWilson67 <143644462+EvaWilson67@users.noreply.github.com>
2025-03-05 14:41:53 -0500

    Project 6

--- csce242/projects/part6/js/house.js
+++ csce242/projects/part6/js/house.js
@@ -19,93 +19,39 @@ const showPlants = async () => {
   
 
   plants.forEach((plant) => {
+    const a = document.createElement("a");
+      a.href = plant.link;
+      a.classList = "links";
+
+      const section = document.createElement("section");
+      section.classList = "segment columns center";
+      a.append(section);
+
+      const divImage = document.createElement("div");
+      divImage.classList.add("height-center");
+      section.append(divImage);
+
+      const img = document.createElement("img");
+      img.src = `images/house_images/${plant.image}`;
+      divImage.append(img);
+
+      const divInfo = document.createElement("div");
+      section.append(divInfo);
+
+      const name = document.createElement("h3");
+      name.innerHTML = plant.name;
+      divInfo.append(name);
+
+      const summary = document.createElement("p");
+      summary.innerHTML = plant.summary;
+      divInfo.append(summary);
+    
     if (plant.section.toLowerCase() == "easy") {
-      const a = document.createElement("a");
-      a.href = plant.link;
-      a.classList = "links";
-
       easySection.append(a);
-
-      const section = document.createElement("section");
-      section.classList = "segment columns center";
-      a.append(section);
-
-      const divImage = document.createElement("div");
-      divImage.classList.add("height-center");
-      section.append(divImage);
-
-      const img = document.createElement("img");
-      img.src = `images/house_images/${plant.image}`;
-      divImage.append(img);
-
-      const divInfo = document.createElement("div");
-      section.append(divInfo);
-
-      const name = document.createElement("h3");
-      name.innerHTML = plant.name;
-      divInfo.append(name);
-
-      const summary = document.createElement("p");
-      summary.innerHTML = plant.summary;
-      divInfo.append(summary);
-    } else if (plant.section.toLowerCase() == "medium") {
-      const a = document.createElement("a");
-      a.href = plant.link;
-      a.classList = "links";
-
+    }else if (plant.section.toLowerCase() == "medium") {
       mediumSection.append(a);
-
-      const section = document.createElement("section");
-      section.classList = "segment columns center";
-      a.append(section);
-
-      const divImage = document.createElement("div");
-      divImage.classList.add("height-center");
-      section.append(divImage);
-
-      const img = document.createElement("img");
-      img.src = `images/house_images/${plant.image}`;
-      divImage.append(img);
-
-      const divInfo = document.createElement("div");
-      section.append(divInfo);
-
-      const name = document.createElement("h3");
-      name.innerHTML = plant.name;
-      divInfo.append(name);
-
-      const summary = document.createElement("p");
-      summary.innerHTML = plant.summary;
-      divInfo.append(summary);
-    } else if (plant.section.toLowerCase() == "hard") {
-      const a = document.createElement("a");
-      a.href = plant.link;
-      a.classList = "links";
-
+    }else if (plant.section.toLowerCase() == "hard") {
       hardSection.append(a);
-
-      const section = document.createElement("section");
-      section.classList = "segment columns center";
-      a.append(section);
-
-      const divImage = document.createElement("div");
-      divImage.classList.add("height-center");
-      section.append(divImage);
-
-      const img = document.createElement("img");
-      img.src = `images/house_images/${plant.image}`;
-      divImage.append(img);
-
-      const divInfo = document.createElement("div");
-      section.append(divInfo);
-
-      const name = document.createElement("h3");
-      name.innerHTML = plant.name;
-      divInfo.append(name);
-
-      const summary = document.createElement("p");
-      summary.innerHTML = plant.summary;
-      divInfo.append(summary);
     }
   });
 };
