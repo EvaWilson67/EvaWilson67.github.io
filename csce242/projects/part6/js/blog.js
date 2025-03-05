7b52230590f6bff93db320b151e611794a9f6a27
EvaWilson67 <143644462+EvaWilson67@users.noreply.github.com>
2025-03-05 11:29:56 -0500

    Project 6

---
+++ csce242/projects/part6/js/blog.js
@@ -0,0 +1,45 @@
+const getBlogs = async () => {
+  const url =
+    "https://evawilson67.github.io/csce242/projects/part6/json/blog.json";
+  try {
+    const response = await fetch(url);
+    return response.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const showBlogs = async () => {
+  const blogs = await getBlogs();
+  const blogSection = document.getElementById("blog");
+  console.log("hi");
+  
+  blogs.forEach((blog) => {
+    console.log(blog);
+  });
+//   blogs.forEach((blog) => {
+//     const section = document.getElementById("section");
+//     section.classList = "blog center";
+//     blogSection.append(section);
+
+//     const div = document.getElementById("div");
+//     div.classList = "columns";
+//     section.append(div);
+
+//     const img = document.createElement("img");
+//     img.src = `images/blog_images/${blog.image}`;
+//     div.append(img);
+
+//     const h3 = document.createElement("h3");
+//     h3.innerHTML = blog.name;
+//     h3.classList = "height-center";
+//     div.append(h3);
+
+//     const p = document.createElement("p");
+//     p.innerHTML = blog.summary;
+//     section.append(p);
+//   })
+  ;
+};
+
+showBlogs();
