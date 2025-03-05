70864bf4122cb9fb6caf7ee3f705cb248aee7712
EvaWilson67 <143644462+EvaWilson67@users.noreply.github.com>
2025-03-05 12:42:18 -0500

    Project 6

--- csce242/projects/part6/js/garden.js
+++ csce242/projects/part6/js/garden.js
@@ -1,11 +1,11 @@
 /*Gardening Stuff*/
-const garden = document.getElementById("hidden-2");
-if (garden) {
-  document.getElementById("hidden-2").onmouseover = () => {
-    document.getElementById("hidden-2").classList.add("unhidden-title");
-  };
+// const garden = document.getElementById("hidden-2");
+// if (garden) {
+//   document.getElementById("hidden-2").onmouseover = () => {
+//     document.getElementById("hidden-2").classList.add("unhidden-title");
+//   };
 
-  document.getElementById("hidden-2").onmouseleave = () => {
-    document.getElementById("hidden-2").classList.remove("unhidden-title");
-  };
-}
+//   document.getElementById("hidden-2").onmouseleave = () => {
+//     document.getElementById("hidden-2").classList.remove("unhidden-title");
+//   };
+// }
