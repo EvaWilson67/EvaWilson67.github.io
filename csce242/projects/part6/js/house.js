7cb36529a2f079a9cb1ac253ea5bd8cecdc46cdb
EvaWilson67 <143644462+EvaWilson67@users.noreply.github.com>
2025-03-05 09:45:51 -0500

    Project 6

--- csce242/projects/part6/js/house.js
+++ csce242/projects/part6/js/house.js
@@ -13,142 +13,100 @@ const getPlants = async () => {
 
 const showPlants = async () => {
   const plants = await getPlants();
-  const housePlants = document.getElementById("house-plants");
-
-//   const easySection = document.createElement("section");
-//   const mediumSection = document.createElement("section");
-//   const hardSection = document.createElement("section");
+  const easySection = document.getElementById("easy-section");
+  const mediumSection = document.getElementById("medium-section");
+  const hardSection = document.getElementById("hard-section");
 
   plants.forEach((plant) => {
-    if(plant.section.toLowerCase()=="easy"){
-        const easySection = document.getElementById("easy-section");
+    if (plant.section.toLowerCase() == "easy") {
+      const a = document.createElement("a");
+      a.href = plant.link;
+      a.classList = "links";
 
-        const a = document.createElement("a");
-        a.href = plant.link;
-        a.classList="links";
+      easySection.append(a);
 
-        easySection.append(a);
+      const section = document.createElement("section");
+      section.classList = "segment columns center";
+      a.append(section);
 
-        const section = document.createElement("section");
-        section.classList="segment columns center";
-        a.append(section);
+      const divImage = document.createElement("div");
+      divImage.classList.add("height-center");
+      section.append(divImage);
 
-        const divImage = document.createElement("div");
-        divImage.classList.add("height-center");
-        section.append(divImage);
+      const img = document.createElement("img");
+      img.src = `images/house_images/${plant.image}`;
+      divImage.append(img);
 
-        const img = document.createElement("img");
-        img.src=`images/house_images/${plant.image}`;
-        divImage.append(img);
+      const divInfo = document.createElement("div");
+      section.append(divInfo);
 
-        const divInfo = document.createElement("div");
-        section.append(divInfo);
+      const name = document.createElement("h3");
+      name.innerHTML = plant.name;
+      divInfo.append(name);
 
+      const summary = document.createElement("p");
+      summary.innerHTML = plant.summary;
+      divInfo.append(summary);
+    } else if (plant.section.toLowerCase() == "medium") {
+      const a = document.createElement("a");
+      a.href = plant.link;
+      a.classList = "links";
 
-        const name = document.createElement("h3");
-        name.innerHTML=plant.name;
-        divInfo.append(name);
+      mediumSection.append(a);
 
-        const summary = document.createElement("p");
-        summary.innerHTML=plant.summary;
-        divInfo.append(summary);
+      const section = document.createElement("section");
+      section.classList = "segment columns center";
+      a.append(section);
 
+      const divImage = document.createElement("div");
+      divImage.classList.add("height-center");
+      section.append(divImage);
+
+      const img = document.createElement("img");
+      img.src = `images/house_images/${plant.image}`;
+      divImage.append(img);
+
+      const divInfo = document.createElement("div");
+      section.append(divInfo);
+
+      const name = document.createElement("h3");
+      name.innerHTML = plant.name;
+      divInfo.append(name);
+
+      const summary = document.createElement("p");
+      summary.innerHTML = plant.summary;
+      divInfo.append(summary);
+    } else if (plant.section.toLowerCase() == "hard") {
+      const a = document.createElement("a");
+      a.href = plant.link;
+      a.classList = "links";
+
+      hardSection.append(a);
+
+      const section = document.createElement("section");
+      section.classList = "segment columns center";
+      a.append(section);
+
+      const divImage = document.createElement("div");
+      divImage.classList.add("height-center");
+      section.append(divImage);
+
+      const img = document.createElement("img");
+      img.src = `images/house_images/${plant.image}`;
+      divImage.append(img);
+
+      const divInfo = document.createElement("div");
+      section.append(divInfo);
+
+      const name = document.createElement("h3");
+      name.innerHTML = plant.name;
+      divInfo.append(name);
+
+      const summary = document.createElement("p");
+      summary.innerHTML = plant.summary;
+      divInfo.append(summary);
     }
-    else if(plant.section.toLowerCase() =="medium"){
-        const mediumSection = document.getElementById("medium-section");
-
-        const a = document.createElement("a");
-        a.href = plant.link;
-        a.classList="links";
-
-        mediumSection.append(a);
-
-        const section = document.createElement("section");
-        section.classList="segment columns center";
-        a.append(section);
-
-        const divImage = document.createElement("div");
-        divImage.classList.add("height-center");
-        section.append(divImage);
-
-        const img = document.createElement("img");
-        img.src=`images/house_images/${plant.image}`;
-        divImage.append(img);
-
-        const divInfo = document.createElement("div");
-        section.append(divInfo);
-
-
-        const name = document.createElement("h3");
-        name.innerHTML=plant.name;
-        divInfo.append(name);
-
-        const summary = document.createElement("p");
-        summary.innerHTML=plant.summary;
-        divInfo.append(summary);
-    }
-    else if(plant.section.toLowerCase() =="hard"){
-        const hardSection = document.getElementById("hard-section");
-
-        const a = document.createElement("a");
-        a.href = plant.link;
-        a.classList="links";
-
-        hardSection.append(a);
-
-        const section = document.createElement("section");
-        section.classList="segment columns center";
-        a.append(section);
-
-        const divImage = document.createElement("div");
-        divImage.classList.add("height-center");
-        section.append(divImage);
-
-        const img = document.createElement("img");
-        img.src=`images/house_images/${plant.image}`;
-        divImage.append(img);
-
-        const divInfo = document.createElement("div");
-        section.append(divInfo);
-
-
-        const name = document.createElement("h3");
-        name.innerHTML=plant.name;
-        divInfo.append(name);
-
-        const summary = document.createElement("p");
-        summary.innerHTML=plant.summary;
-        divInfo.append(summary);
-    }
-
-
   });
-
-  // const shoes = await getShoes();
-  // const shoesSection = document.getElementById("shoes-section");
-
-  // shoes.forEach((shoe)=>{
-  //     const section = document.createElement("section");
-  //     shoesSection.append(section);
-  //     const h3 = document.createElement("h3");
-  //     h3.innerHTML = shoe.name;
-  //     section.append(h3);
-
-  //     const p = document.createElement("p");
-  //     section.append(p);
-  //     p.innerHTML = `Brand: ${shoe.brand}`
-
-  //     const ul = document.createElement("ul");
-  //     section.append(ul);
-
-  //     //loop through each review
-  //     shoe.reviews.forEach((review)=>{
-  //         const li = document.createElement("li");
-  //         li.append(review);
-  //         ul.append(li);
-  //     });
-
-  // });
 };
 
 showPlants();
